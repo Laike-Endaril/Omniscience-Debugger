5dcea86b7a831a7e88e374495552e1132696c3c1
Laike-Endaril <silvertallon@gmail.com>
2025-05-02 22:57:02 -0700

    1.12.2.003

--- src/main/java/com/fantasticsource/omniscience/Omniscience.java
+++ src/main/java/com/fantasticsource/omniscience/Omniscience.java
@@ -30,7 +30,7 @@ public class Omniscience
 {
     public static final String MODID = "omnisciencedebugger";
     public static final String NAME = "Omniscience Debugger";
-    public static final String VERSION = "1.12.2.002c";
+    public static final String VERSION = "1.12.2.003";
 
 
     static
