09a49624740782ad3299b498997dcfe5aaeba45d
Laike-Endaril <silvertallon@gmail.com>
2025-05-13 11:00:31 -0700

    1.12.2.004

--- src/main/java/com/fantasticsource/omniscience/Omniscience.java
+++ src/main/java/com/fantasticsource/omniscience/Omniscience.java
@@ -30,7 +30,7 @@ public class Omniscience
 {
     public static final String MODID = "omnisciencedebugger";
     public static final String NAME = "Omniscience Debugger";
-    public static final String VERSION = "1.12.2.003";
+    public static final String VERSION = "1.12.2.004";
 
 
     public static final OmniEventBus OMNI_EVENT_BUS = new OmniEventBus(MinecraftForge.EVENT_BUS);
